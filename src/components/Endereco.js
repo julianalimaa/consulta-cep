7f5820dfa7351ba1538de20d507d4bc645814975
Juliana Lima <jugalvao87@gmail.com>
2021-04-15 20:13:55 -0300

    Adicionando state no campo CEP

--- src/components/Endereco.js
+++ src/components/Endereco.js
@@ -1,27 +1,29 @@
 import { TextField } from "@material-ui/core"
 import './Endereco.css'
+import { useState } from 'react'
 
-const CampoTextoCustomizado = ({label}) => {
+const CampoTextoCustomizado = ({label, manipuladorOnChange}) => {
+
     return (
         <div className="encapsuladorCampoTexto">
-            <TextField label={label} variant="filled" className="campoTexto" />
-        </div>
-        
+            <TextField onChange={(event)=> manipuladorOnChange(event.target.value)} label={label} variant="filled" className="campoTexto" />
+        </div> 
     )
 }
 
 const Endereco = () => {
+    
+    const [cep, setCep] = useState('')
 
-    return (
+    return (  
         <div className="campoEndereco">
-           <CampoTextoCustomizado label="CEP"/> 
+           <CampoTextoCustomizado manipuladorOnChange={setCep} label="CEP"/> 
            <CampoTextoCustomizado label="Logradouro"/> 
            <CampoTextoCustomizado label="Complemento"/> 
            <CampoTextoCustomizado label="Bairro"/> 
            <CampoTextoCustomizado label="Localidade"/> 
            <CampoTextoCustomizado label="UF"/> 
-        </div>
-        
+        </div>   
     )
 }
 
