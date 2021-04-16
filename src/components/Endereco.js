960e22ed17c1e80965d2d0d82db183fd7813c337
Juliana Lima <jugalvao87@gmail.com>
2021-04-15 23:49:14 -0300

    Implementando requisicao da API

--- src/components/Endereco.js
+++ src/components/Endereco.js
@@ -2,27 +2,63 @@ import { TextField } from "@material-ui/core"
 import './Endereco.css'
 import { useState } from 'react'
 
-const CampoTextoCustomizado = ({label, manipuladorOnChange}) => {
+const CampoTextoCustomizado = ({valor, label, mensagemErro, manipuladorOnChange, manipuladorOnBlur}) => {
 
     return (
         <div className="encapsuladorCampoTexto">
-            <TextField onChange={(event)=> manipuladorOnChange(event.target.value)} label={label} variant="filled" className="campoTexto" />
+            <TextField 
+                value={valor}
+                onBlur={manipuladorOnBlur}
+                onChange={(event)=> 
+                    manipuladorOnChange(event.target.value)} 
+                label={label} 
+                variant="filled" 
+                className="campoTexto"
+                error={mensagemErro}
+                />
         </div> 
     )
 }
 
-const Endereco = () => {
-    
+const Endereco = () => {    
+    const [mensagemErrorCep, setMensagemErrorCep] = useState('')
     const [cep, setCep] = useState('')
+    const [logradouro, setLogradouro] = useState('')
+    const [complemento, setComplemento] = useState('')
+    const [bairro, setBairro] = useState('')
+    const [localidade, setLocalidade] = useState('')
+    const [uf, setUf] = useState('')
+
+    const manipularOnBlurCep = async () => {
+       
+        try {
+            const urlApi = `https://viacep.com.br/ws/${cep}/json/`
+            const resultadoApi = await fetch(urlApi)
+            const json = await resultadoApi.json()
+        
+            if (json.erro) {
+            throw new Error()
+            }
+
+            setLogradouro(json.logradouro)
+            setComplemento(json.complemento)
+            setBairro(json.bairro)
+            setLocalidade(json.localidade)
+            setUf(json.uf)
+
+        } catch (error) {
+            alert('Cep inexistente/inválido!')
+        }
+    }
 
     return (  
         <div className="campoEndereco">
-           <CampoTextoCustomizado manipuladorOnChange={setCep} label="CEP"/> 
-           <CampoTextoCustomizado label="Logradouro"/> 
-           <CampoTextoCustomizado label="Complemento"/> 
-           <CampoTextoCustomizado label="Bairro"/> 
-           <CampoTextoCustomizado label="Localidade"/> 
-           <CampoTextoCustomizado label="UF"/> 
+           <CampoTextoCustomizado mensagemErro={mensagemErrorCep} manipuladorOnChange={setCep} manipuladorOnBlur={manipularOnBlurCep} label="CEP"/> 
+           <CampoTextoCustomizado valor={logradouro} label="Logradouro"/> 
+           <CampoTextoCustomizado valor={complemento} label="Complemento"/> 
+           <CampoTextoCustomizado valor={bairro} label="Bairro"/> 
+           <CampoTextoCustomizado valor={localidade} label="Localidade"/> 
+           <CampoTextoCustomizado valor={uf} label="UF"/> 
         </div>   
     )
 }
